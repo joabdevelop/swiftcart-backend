79e2cdfb0526fd38639e14457ec7f7bb10bbac63
joabdevelop <joabdeveloper@gmail.com>
2026-07-01 15:36:29 -0300

    Service Layer, Component Registration.

--- src/main/java/com/joabdevelop/swiftcart/resources/UserResource.java
+++ src/main/java/com/joabdevelop/swiftcart/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.joabdevelop.swiftcart.resources;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joabdevelop.swiftcart.entities.User;
+import com.joabdevelop.swiftcart.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    private final UserService service;
+
+    public UserResource(UserService service) {
+        this.service = service;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "123456");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/joabdevelop/swiftcart/services/UserService.java
@@ -0,0 +1,29 @@
+package com.joabdevelop.swiftcart.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.joabdevelop.swiftcart.entities.User;
+import com.joabdevelop.swiftcart.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
