f427368f96619a85d1051525295a8ac3c4835536
joabdevelop <joabdeveloper@gmail.com>
2026-07-02 09:32:25 -0300

    Many-to-Many Association With Extra Attributes

--- src/main/java/com/joabdevelop/swiftcart/config/TestConfig.java
+++ src/main/java/com/joabdevelop/swiftcart/config/TestConfig.java
@@ -39,14 +39,7 @@ public class TestConfig implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Category c1 = new Category(null, "Electronics");
-        Category c2 = new Category(null, "Books");
-        Category c3 = new Category(null, "Computers");
-
-        categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
-
-        User u1 = new User(null, "Maria Brown", "[EMAIL_ADDRESS]", "988888888", "123456");
+    public void run(String... args) throws Exception { User u1 = new User(null, "Maria Brown", "[EMAIL_ADDRESS]", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "[EMAIL_ADDRESS]", "988888888", "123456");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
@@ -57,11 +50,26 @@ public class TestConfig implements CommandLineRunner {
 
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+        Category c1 = new Category(null, "Electronics");
+        Category c2 = new Category(null, "Books");
+        Category c3 = new Category(null, "Computers");
+
+        categoryRepository.saveAll(Arrays.asList(c1, c2, c3));
+
         Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
         Product p2 = new Product(null, "Smart TV", "Apple TV", 1000.0, "");
         Product p3 = new Product(null, "Macbook Pro", "Apple Macbook Pro", 1990.5, "");
         Product p4 = new Product(null, "PC Gamer", "ASUS ROG", 3990.5, "");
         Product p5 = new Product(null, "Rails for Dummies", "Learn Ruby on Rails", 20.5, "");
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        p1.getCategories().add(c2);
+        p2.getCategories().add(c1);
+        p2.getCategories().add(c3);
+        p3.getCategories().add(c3);
+        p4.getCategories().add(c3);
+        p5.getCategories().add(c2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
     }
